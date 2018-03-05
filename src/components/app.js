4c63006b794b7b49b1e355849db87f3845f15121
Don Bozarth <don@fuelcnc.com>
2018-03-05 14:42:16 -0600

    Removed some files for cleanup

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
-import CommentBox from './comment_box';
-import CommentList from './comment_list';
-
 export default class App extends Component {
   render() {
     return (
       <div>
         <h3>React simple starter</h3>
-        <CommentBox />
-        <CommentList />
       </div>     
     );
   }
